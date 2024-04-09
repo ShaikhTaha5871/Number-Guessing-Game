482e5ce5cbb1f33dc190cf95946db6bc5bdaf5dd
ShaikhTaha5871 <164833275+ShaikhTaha5871@users.noreply.github.com>
2024-04-09 09:10:02 +0500

    added a console

--- index.ts
+++ index.ts
@@ -9,13 +9,15 @@ import inquirer from "inquirer";
 
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 
+console.log("Welcome to number Guessing Game");
+
 const answers = await inquirer.prompt([
   {
     name: "userGuessedNumber",
     type: "number",
     message: "plese guess a number between 1-6: ",
   },
-]);
+]);   
 
 if (answers.userGuessedNumber === randomNumber) {
   console.log("Congtratulations! You guessed right number.");
